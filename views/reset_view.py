881347b7e0384c5725fa0d3dbc5dce32bc942f44
dieginin <diegobalestra9@gmail.com>
2025-02-06 01:58:18 -0800

    update reset_view.py -> reset view finished

--- views/reset_view.py
+++ views/reset_view.py
@@ -1,6 +1,9 @@
+import re
+
 import flet as ft
 
-from components import PrimaryButton
+from components import EmailField, PrimaryButton
+from controllers import Firebase, error_snackbar, success_snackbar
 
 
 class ResetView(ft.View):
@@ -8,20 +11,51 @@ class ResetView(ft.View):
         super().__init__()
         self.route = "/reset"
 
+        self.fb = Firebase()
+
         self.__init_components__()
 
     def __init_components__(self) -> None:
+        self.logo = ft.Image(
+            src="logo.png",
+            width=300,
+            height=300,
+            animate_size=ft.animation.Animation(500, ft.AnimationCurve.EASE_OUT_BACK),
+        )
+        title = ft.Text("Please enter an email", size=40, color="outline")
+
+        self.email = EmailField("Email", on_submit=self.__reset_password)
+
         self.controls = [
             ft.SafeArea(
                 ft.Column(
                     [
-                        ft.Text("Reset"),
+                        self.logo,
+                        title,
+                        self.email,
                         PrimaryButton("Reset password", on_click=self.__reset_password),
-                    ]
-                )
+                    ],
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    spacing=35,
+                    scroll=ft.ScrollMode.HIDDEN,
+                ),
+                expand=True,
             )
         ]
 
     def __reset_password(self, e: ft.ControlEvent) -> None:
-        e.page.go("/login")
-        e.page.update()
+        email = self.email.value
+        if not email:
+            return self.email.focus()
+
+        if not re.match(r"[^@\s]+@[^@\s]+\.[a-zA-Z0-9]+$", email):
+            error_snackbar(e.page, "Enter a valid email")
+            return self.email.focus()
+        response = self.fb.reset_password(email)
+
+        if response.status == "Success":
+            e.page.go("/login")
+            e.page.update()
+            success_snackbar(e.page, response.message)
+        else:
+            error_snackbar(e.page, response.message)
