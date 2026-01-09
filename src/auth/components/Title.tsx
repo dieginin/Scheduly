8e79a1de1ff5417628c2c2a88faf259a66c21e82
dieginin <diegobalestra9@gmail.com>
2026-01-08 20:11:55 -0800

    refactor: add optional withoutLogo prop to Title component for conditional logo rendering

--- src/auth/components/Title.tsx
+++ src/auth/components/Title.tsx
@@ -1,12 +1,13 @@
 interface Props {
   subtitle: string
   title: string
+  withoutLogo?: boolean
 }
 
-export const Title = ({ subtitle, title }: Props) => {
+export const Title = ({ subtitle, title, withoutLogo = false }: Props) => {
   return (
     <>
-      <img className='w-35 h-35 mx-auto' src='/images/logo.svg' alt='Scheduly Logo' />
+      {!withoutLogo && <img className='w-35 h-35 mx-auto' src='/images/logo.svg' alt='Scheduly Logo' />}
       <h1 className='text-4xl text-foreground font-semibold text-center'>{title}</h1>
       <p className='text-xl font-thin text-center text-muted-foreground'>{subtitle}</p>
     </>
