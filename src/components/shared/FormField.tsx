463409f2a36570979ebc4f77bb41906d66032b19
dieginin <diegobalestra9@gmail.com>
2026-01-08 20:10:01 -0800

    refactor: enhance type safety in FormField component by using generic Props

--- src/components/shared/FormField.tsx
+++ src/components/shared/FormField.tsx
@@ -1,20 +1,19 @@
-import type { ControllerFieldState, ControllerRenderProps } from "react-hook-form"
+import type { ControllerFieldState, ControllerRenderProps, FieldValues, Path } from "react-hook-form"
 import { Field, FieldError, FieldLabel } from "@/components/ui/field"
 
 import type { HTMLInputTypeAttribute } from "react"
 import { Input } from "@/components/ui/input"
-import type { LoginFormData } from "@/auth/validations"
 
-interface Props {
-  field: ControllerRenderProps<LoginFormData>
+interface Props<T extends FieldValues> {
+  field: ControllerRenderProps<T, Path<T>>
   fieldState: ControllerFieldState
   label: string
-  name: string
+  name: Path<T>
   placeholder: string
   type?: HTMLInputTypeAttribute
 }
 
-export const FormField = ({ field, fieldState, label, name, placeholder, type }: Props) => {
+export const FormField = <T extends FieldValues>({ field, fieldState, label, name, placeholder, type }: Props<T>) => {
   return (
     <Field data-invalid={fieldState.invalid} className='grid gap-1'>
       <FieldLabel htmlFor={name} children={label} />
