64b2397150e3db995cde51b5ae174face5439d12
dieginin <diegobalestra9@gmail.com>
2026-01-10 01:32:04 -0800

    fix: adjust BackButton position to align correctly on the right

--- src/app/components/BackButton.tsx
+++ src/app/components/BackButton.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router"
 
 export const BackButton = () => {
   return (
-    <Link to='/' className='border-background fixed top-4 left-4 overflow-hidden rounded-sm border p-0.5 backdrop-blur-sm'>
+    <Link to='/' className='border-background fixed top-4 right-4 overflow-hidden rounded-sm border p-0.5 backdrop-blur-sm'>
       <Button variant='link'>
         <ChevronLeft className='-mr-1' />
         Go back
