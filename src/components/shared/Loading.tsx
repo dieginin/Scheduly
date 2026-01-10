881d3e02f9a2eb0f2c648d9d5caaa5f4a88def7e
dieginin <diegobalestra9@gmail.com>
2026-01-10 03:09:14 -0800

    fix: reorder class names in Loading component for improved readability

--- src/components/shared/Loading.tsx
+++ src/components/shared/Loading.tsx
@@ -2,7 +2,7 @@ import { LoaderCircle } from "lucide-react"
 
 export const Loading = () => {
   return (
-    <div className='min-h-svh flex flex-col justify-center items-center gap-2'>
+    <div className='flex flex-col items-center justify-center gap-2 min-h-svh'>
       <LoaderCircle className='animate-spin w-27.5 h-27.5 text-primary/20' />
       <h1 className='text-4xl font-thin'>Loading...</h1>
     </div>
